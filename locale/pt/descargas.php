f2faa18aeac18e23eb7f637530638fc150bb0011
zap2675 <zap2675@gmail.com>
2016-08-12 20:07:33 -0300

    Update descargas.php

--- locale/pt/descargas.php
+++ locale/pt/descargas.php
@@ -4,7 +4,7 @@
 
 <div class="col-3">
 <img src="/img/iconos/hd-imagen.png" alt="hd">
-<h4><a class="lbp-inline-link-5 cboxElement" href="#">Alta Definición – HD</a></h4>
+<h4><a class="lbp-inline-link-5 cboxElement" href="#">Alta Definição – HD</a></h4>
 <p><b>1920×1080 – 4.3Gb</b></p>
 <hr>
 <p><small>Máxima qualidade de imagem. Ideal para projeções, dispositivos de alta definição, HDTV e <em>broadcasting</em>.</small></p>
@@ -15,75 +15,75 @@
 <h4><a class="lbp-inline-link-6 cboxElement" href="#">Definição standard – SD</a></h4>
 <p><b>854×480 – 1.4Gb</b></p>
 <hr>
-<p><small>Qualidade muito boa. Para netbooks, tablets y computadoras antiguas. Apto para TV analógica.</small></p>
+<p><small>Qualidade muito boa. Para netbooks, tablets e computadores antigos. Apto para TV análoga.</small></p>
 </div>
 
 <div class="col-3" style="border: none;">
 <img src="/img/iconos/mv-imagen.png" alt="movil">
-<h4><a class="lbp-inline-link-7 cboxElement" href="#">Versión móvil</a></h4>
+<h4><a class="lbp-inline-link-7 cboxElement" href="#">Versão móvil</a></h4>
 
 <p><b>480×272 – 691Mb</b></p>
 <hr>
-<p><small>Ideal para dispositivos móviles como teléfonos celulares, reproductores de video y consolas de video juego portátiles.</small></p>
+<p><small>Ideal para dispositivos móveis como telefones celulares, reprodutores de vídeo e consoles de videogame portáteis.</small></p>
 </div>
 
-<h4>Otras versiones</h4>
+<h4>Outras versões</h4>
 <ul style="width: 100%;">
 <li><a href="https://rapidshare.com/#!download|647p6|1383928579|La%20Educaci%C3%B3n%20Prohibida%20-%20Pel%C3%ADcula%20Completa%20HD%28720p_H.264-AAC%29.mp4|2173402|0|0">Versión 720p</a></li>
 <li><a href="https://rapidshare.com/#!download|96p12|5281943|La%20Educaci%C3%B3n%20Prohibida%20-%20Pel%C3%ADcula%20Completa%20HD%28360p_H.264-AAC%29.mp4|645003|0|0">Versión 360p</a></li>
 </ul>
 <h3>Subtítulos</h3>
-<p>Los subtitulos disponibles (en format SRT) actualmente son los siguientes:</p>
+<p>Os subtitulos disponíveis (em formato SRT) atualmente são os seguintes:</p>
 <?php include('include/subs.php'); ?>
 </ul>
-<h2>Versión de sala</h2>
-<p>Versión adaptada para su proyección en salas de cine y afines, reducida en su duración. Incluye un video introductorio al proyecto por parte de German Doin. Duración: 02:00:15. Se encuentra disponible en 2 formatos:<br>
+<h2>Versão de sala</h2>
+<p>Versão adaptada para sua projeção em salas de cinema e afins, reduzida na sua duração. Inclui um vídeo introdutório ao projeto de parte de German Doin. Duração: 02:00:15. Está disponível em 2 formatos:<br>
 </p>
 
 <div class="col-3">
 <img src="/img/iconos/hd-imagen.png" alt="hd">
-<h4><a class="lbp-inline-link-8 cboxElement" href="#">Alta Definición – HD</a></h4>
+<h4><a class="lbp-inline-link-8 cboxElement" href="#">Alta Definição – HD</a></h4>
 <p><b>1920×1080 – 2.3Gb</b></p>
 <hr>
-<p><small>Apta para usar con subtítulos.</small></p>
+<p><small>Apta para usar com legendas.</small></p>
 </div>
 
 <div class="col-3">
 <img src="/img/iconos/dvd-imagen.png" alt="sd">
-<h4><a class="lbp-inline-link-9 cboxElement" href="#">Versión en DVD</a></h4>
+<h4><a class="lbp-inline-link-9 cboxElement" href="#">Versão em DVD</a></h4>
 <p><b>720×480 – 4.4Gb</b></p>
 <hr>
-<p><small>Calidad de DVD (menor que HD). No incluye subtítulos.</small></p>
+<p><small>Qualidade de DVD (menor que HD). Não inclui legendas.</small></p>
 </div>
 
-<h3>Subtítulos</h3>
+<h3>Legendas</h3>
   <ul style="clear: both; margin-bottom: 35px; width: 100%;">
     <li><a href="/datos/pelicula/sala/subtitulos/subtitulos.php?v=es_ES">Español</a></li>
     <li><a href="/datos/pelicula/sala/subtitulos/subtitulos.php?v=en_EN">Inglés / English</a></li>
     <li><a href="/datos/pelicula/sala/subtitulos/subtitulos.php?v=pt_BR">Portugés / Português</a></li>
   </ul>
 
-<h2>Diseños para Impresión</h2>
-  <p>Aquí compartirmos los diseños de impresión originales creados para la primer edición de la película.</p>
+<h2>Desenhos para Impressão</h2>
+  <p>Aquí compartilharemos os desenhos de impressão originais criados para a primeira edição do filme.</p>
   <ul style="clear: both; margin-bottom: 35px; width: 100%;">
-    <li><a title="Tigrafías / Fonts" href="/datos/grafica/tipografias/">Tigrafías / Fonts</a></li>
-    <li><a title="Diseño de Tapa" href="/datos/grafica/dvd/tapa-dvd-simple.pdf">Diseño de Tapa</a></li>
-    <li><a title="Diseño de Tapa Editable" href="/datos/grafica/dvd/tapa-dvd-simple.svg">Diseño de Tapa</a> – Editable en SVG</li>
-    <li><a title="Diseño de Tapa con Subtítulos" href="/datos/grafica/dvd/tapa-dvd-sub.png">Diseño de Tapa con Subtítulos</a></li>
-    <li><a title="Diseño de Tapa con Subtítulos Editable" href="/datos/grafica/dvd/tapa-dvd-sub.svg">Diseño de Tapa con Subtítulos</a> – Editable en SVG</li>
-    <li><a title="Diseño de Disco" href="/datos/grafica/dvd/dvd-estampado.pdf">Diseño de Disco DVD</a></li>
-    <li><a title="Diseño de Disco" href="/datos/grafica/dvd/dvd-estampado.svg">Diseño de Disco DVD</a> – Editable en SVG</li>
+    <li><a title="Tigrafías / Fonts" href="/datos/grafica/tipografias/">Tipografias / Fonts</a></li>
+    <li><a title="Diseño de Tapa" href="/datos/grafica/dvd/tapa-dvd-simple.pdf">Desenho de Capa</a></li>
+    <li><a title="Diseño de Tapa Editable" href="/datos/grafica/dvd/tapa-dvd-simple.svg">Desenho de Capa</a> – Editável em SVG</li>
+    <li><a title="Diseño de Tapa con Subtítulos" href="/datos/grafica/dvd/tapa-dvd-sub.png">Desenho de Capa com Legendas</a></li>
+    <li><a title="Diseño de Tapa con Subtítulos Editable" href="/datos/grafica/dvd/tapa-dvd-sub.svg">Desenho de Capa com Legendas</a> – Editável em SVG</li>
+    <li><a title="Diseño de Disco" href="/datos/grafica/dvd/dvd-estampado.pdf">Desenho de Disco DVD</a></li>
+    <li><a title="Diseño de Disco" href="/datos/grafica/dvd/dvd-estampado.svg">Desenho de Disco DVD</a> – Editável em SVG</li>
   </ul>
 
-<h2>Ayuda</h2>
+<h2>Ajuda</h2>
 
-<h3 id="como">¿Cómo descargo mediante Torrent?</h3>
+<h3 id="como">¿Como faço o download através de Torrent?</h3>
 
   <ol style="float: left; width: 800px;">
-    <li>Baja e instala en tu equipo un cliente de BitTorrent como <a href="http://www.utorrent.com/intl/es/downloads/">uTorrent</a> o <a href="http://azureus.sourceforge.net/">Vuze</a>.</li>
-    <li>Descarga el archivo torrent de la pelicula desde estos enlaces: <a href="/datos/pelicula/completa/la_educacion_prohibida-completa-1080p.mp4.torrent">HD Completa</a> y <a href="/datos/pelicula/completa/la_educacion_prohibida-completa-480p.mp4.torrent">SD Completa</a></li>
-    <li>Carga el archivo BitTorrent en tu cliente y comienza la descarga.</li>
-    <li>No cierres tu cliente de BitTorrent cuando hayas terminado la descarga, de esa forma ayudarás a otros que también puedan descargarla.”</li>
+    <li>Faça o download e instale no teu equipamento um cliente de BitTorrent como <a href="http://www.utorrent.com/intl/es/downloads/">uTorrent</a> ou <a href="http://azureus.sourceforge.net/">Vuze</a>.</li>
+    <li>Descarregue o arquivo torrent do filme desses enlaces: <a href="/datos/pelicula/completa/la_educacion_prohibida-completa-1080p.mp4.torrent">HD Completo</a> e <a href="/datos/pelicula/completa/la_educacion_prohibida-completa-480p.mp4.torrent">SD Completa</a></li>
+    <li>Carregue o arquivo BitTorrent no teu cliente e comece a descarga.</li>
+    <li>Não feche o teu cliente de BitTorrent quando se termine a descarga. Dessa forma você estará ajudando a outros para que também possam descarregá-la.”</li>
   </ol>
 
 <?php include('include/popup.php') ?>
