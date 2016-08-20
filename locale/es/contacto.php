9764baf21981f5b64006265380214cc8e1cf278c
Yaco <franco@redesdepares.org>
2016-08-20 01:58:18 -0300

    agrega pagina de contacto, solo con email

---
+++ locale/es/contacto.php
@@ -0,0 +1,1 @@
+<p>Ante cualquier duda o consulta, escribenos por correo electrónico a <b>info(arroba)educacionprohibida.com</b>. Contestaremos a la brevedad.</p>
---
+++ locale/pt/contacto.php
@@ -0,0 +1,1 @@
+<p>Para qualquer dúvida, por favor escreva-nos pelo e-mail <b>info(at)educacaoproibida.com</b>. Nós responderemos em breve.</p>
