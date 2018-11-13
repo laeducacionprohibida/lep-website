8f45f6e827b97d49fee4c32f60fcaea06a132b27
Ger Doin <germandoin@gmail.com>
2018-11-13 19:56:38 -0300

    Update header.php

--- tpl/header.php
+++ tpl/header.php
@@ -1,7 +1,6 @@
 <div id="cabecera">
 
   <a id="cab_logo" href="/"><img src="/img/logo.<?php echo $lang; ?>.png" /></a>
-  <a id="cab_logo_reevo" href="http://reevo.org"></a>
   <div id="caja-social">
     <a target="_blank" id="btn_twitter" class="btn_social" href="http://twitter.com/edprohibida/" title="<?php echo _('Siguenos en Twitter');?>"></a>
     <a target="_blank" id="btn_facebook" class="btn_social" href="http://www.facebook.com/LAEDUCACIONPROHIBIDA" title="<?php echo _('Siguenos en Facebook');?>" ></a>
